c636adf6e36e9e2005a2131b6cee4afd1104d772
Imam Taufiqurrokhman <imamtaufiqurrokhman@gmail.com>
2022-03-21 12:15:49 +0700

    IMplement UserDetails and UserDetailsService

---
+++ src/main/java/com/itforshort/toko/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.itforshort.toko.security.services;
+
+import com.itforshort.toko.model.User;
+import com.itforshort.toko.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    
+    public final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return UserDetailsImpl.build(user);
+    }
+}
