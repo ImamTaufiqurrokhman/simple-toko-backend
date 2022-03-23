fd36f99dfd3f47196d5842141dd632fea672864d
Imam Taufiqurrokhman <imamtaufiqurrokhman@gmail.com>
2022-03-23 19:48:59 +0700

    Add ErrorResponse Payload

---
+++ src/main/java/com/itforshort/toko/payload/response/ErrorResponse.java
@@ -0,0 +1,33 @@
+package com.itforshort.toko.payload.response;
+
+import java.util.Map;
+
+public class ErrorResponse {
+    private int status;
+    private String error;
+    private Map<String, String> errors;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(Map<String, String> errors) {
+        this.errors = errors;
+    }
+}
