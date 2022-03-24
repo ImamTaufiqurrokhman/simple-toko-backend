4d263218fa0abe2e75560c145088e40cf57cbc4d
Imam Taufiqurrokhman <imamtaufiqurrokhman@gmail.com>
2022-03-24 20:56:59 +0700

    Implement role based authentication for authorization to endpoint in register route + request and user details implementation

--- src/main/java/com/itforshort/toko/controller/AuthController.java
+++ src/main/java/com/itforshort/toko/controller/AuthController.java
@@ -1,10 +1,13 @@
 package com.itforshort.toko.controller;
 
+import com.itforshort.toko.model.ERole;
+import com.itforshort.toko.model.Role;
 import com.itforshort.toko.model.User;
 import com.itforshort.toko.payload.request.LoginRequest;
 import com.itforshort.toko.payload.request.RegisterUserRequest;
 import com.itforshort.toko.payload.response.JwtResponse;
 import com.itforshort.toko.payload.response.MessageResponse;
+import com.itforshort.toko.repository.RoleRepository;
 import com.itforshort.toko.repository.UserRepository;
 import com.itforshort.toko.security.jwt.JwtUtils;
 import com.itforshort.toko.security.services.UserDetailsImpl;
@@ -13,11 +16,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -27,12 +35,14 @@ public class AuthController {
     private final UserRepository userRepository;
     private final PasswordEncoder encoder;
     private final JwtUtils jwtUtils;
+    private final RoleRepository roleRepository;
 
-    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, PasswordEncoder encoder, JwtUtils jwtUtils) {
+    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, PasswordEncoder encoder, JwtUtils jwtUtils, RoleRepository roleRepository) {
         this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
         this.encoder = encoder;
         this.jwtUtils = jwtUtils;
+        this.roleRepository = roleRepository;
     }
 
     @PostMapping("/login")
@@ -44,7 +54,9 @@ public class AuthController {
             String jwt = jwtUtils.generateJwtToken(authentication);
 
             UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-
+            List<String> roles = userDetails.getAuthorities().stream()
+				.map(GrantedAuthority::getAuthority)
+				.collect(Collectors.toList());
             return ResponseEntity.ok(new JwtResponse(jwt,
                                                      userDetails.getId(),
                                                      userDetails.getUsername()));
@@ -64,6 +76,33 @@ public class AuthController {
 
             User user = new User(registerUserRequest.getUsername(),
                                  encoder.encode(registerUserRequest.getPassword()));
+            Set<String> strRoles = registerUserRequest.getRole();
+            Set<Role> roles = new HashSet<>();
+            if (strRoles == null) {
+                Role userRole = roleRepository.findByName(ERole.ROLE_CASHIER)
+                        .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                roles.add(userRole);
+            } else {
+                strRoles.forEach(role -> {
+                    switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+                        break;
+                    case "op":
+                        Role modRole = roleRepository.findByName(ERole.ROLE_OPERATOR)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(modRole);
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName(ERole.ROLE_CASHIER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                    }
+                });
+            }
+            user.setRoles(roles);
             userRepository.save(user);
             return new ResponseEntity<>(new MessageResponse("User registered successfully!"), HttpStatus.CREATED);
         } catch (Exception e) {
--- src/main/java/com/itforshort/toko/payload/request/RegisterUserRequest.java
+++ src/main/java/com/itforshort/toko/payload/request/RegisterUserRequest.java
@@ -1,8 +1,11 @@
 package com.itforshort.toko.payload.request;
 
+import com.itforshort.toko.model.Role;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Set;
 
 public class RegisterUserRequest {
     @NotNull
@@ -13,6 +16,7 @@ public class RegisterUserRequest {
     @Size(min = 9, max = 40)
     private String password;
 
+    private Set<String> role;
 
     public String getUsername() {
         return username;
@@ -28,5 +32,13 @@ public class RegisterUserRequest {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Set<String> getRole() {
+        return role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
     }
 }
